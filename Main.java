04b8a6b1782b7c2b5c3775e2d31fb0739f2b495c
Steven <steven.smith@uconn.edu>
2014-04-08 00:49:08 -0400

    Created Main from TestWindow class
    Made a new version of TestWindow called Main. Made the toolbar show up
    and activated the selection tool. Deactivated many drawing tools in
    DrawApplicationModel.

---
+++ Main.java
@@ -0,0 +1,93 @@
+
+
+	
+	package Project2102;
+
+	import org.jhotdraw.util.*;
+
+import java.awt.Color;
+import java.awt.Rectangle;
+import java.util.*;
+import javax.swing.*;
+import org.jhotdraw.app.*;
+import org.jhotdraw.draw.*;
+import org.jhotdraw.draw.action.*;
+import org.jhotdraw.samples.draw.*;
+
+import test2102.TestWindow;
+import static org.jhotdraw.draw.AttributeKeys.*;
+
+	public class Main extends DrawApplicationModel {
+
+		public Main(){
+		super();		
+		}
+		
+		private DefaultDrawingEditor getEditor(Project pr){
+			DrawProject p = (DrawProject) pr;
+		    
+		    if (p == null) {
+		        return getSharedEditor();
+		    } else {
+		        return (DefaultDrawingEditor) p.getEditor();
+		    }
+		}
+		
+		@Override
+		public List<JToolBar> createToolBars(Application a, Project pr) {
+			DefaultDrawingEditor editor = getEditor(pr);
+			
+			ResourceBundleUtil labels = ResourceBundleUtil.getLAFBundle("org.jhotdraw.draw.Labels");
+			
+			List<JToolBar> toolBars = new LinkedList<JToolBar>();
+			JToolBar newBar = new JToolBar();
+			
+		
+			super.addDefaultCreationButtonsTo(newBar, editor,
+					ToolBarButtonFactory.createDrawingActions(editor), 
+	                ToolBarButtonFactory.createSelectionActions(editor)
+	                );
+			
+			Map<AttributeKey,Object> attributeMap = new HashMap<AttributeKey, Object>();
+			attributeMap.put(AttributeKeys.FILL_COLOR, Color.RED);
+			attributeMap.put(AttributeKeys.STROKE_COLOR, Color.BLUE);
+	                
+//			ToolBarButtonFactory.addStrokeButtonsTo(newBar, super.getSharedEditor());
+			
+			
+			
+			 
+			//put your own button into newBar here. Perhaps you need to extend
+			//ToolBarButtonFactory with your own class to make a factory that produces
+			//custom buttons? Or do you want to add all the code to create and register
+			//a button here? 
+			toolBars.add(newBar);
+			return toolBars;
+		}
+		//How does the toolbar button factory work? Open it up in the draw.action package.
+		//show the addStrokeWidthButtonTo method. 
+		// It first creates a button object, then adds an AttributeAction to the button. 
+		// This action defines the functionality of the button.
+		// There are a bunch of actions defined in the draw.action package. 
+		
+		//Go back to DrawApplicationModel.java and look at the addDefaultCreationButtonsTo class. 
+		//  Here is where you create buttons for establishing Tools: a 'mode of drawing'. So if you want
+		//  to free draw, you need a tool for that; if you want to create a rectangle, you need to activate
+		//  a tool for that too. 
+
+		public static void main(String[] args){
+			//Application app = new DefaultOSXApplication();
+			Application app = new AppWindow();
+			
+			DrawApplicationModel myProject = new Main();
+			app.setModel(myProject);
+			myProject.setName("Test");
+			myProject.setProjectClassName("org.jhotdraw.samples.draw.DrawProject");
+			app.launch(args);
+		
+		
+		}
+		
+	}
+
+
